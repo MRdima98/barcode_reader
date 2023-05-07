32121a20da5e1e2fbc08ca5a1a817996ab58fd65
dima <dimafrunza69@gmail.com>
2023-05-07 08:51:48 +0200

    Extension adds text on click after get request

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,5 +1,12 @@
 document.addEventListener('click', function(e){
     var clickedElement = e.target;
-    clickedElement.innerHTML = "Hello";
-    console.log(clickedElement);
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'http://127.0.0.1:8000/', false ); 
+    xmlHttp.setRequestHeader("X-PINGOTHER", "pingpong");
+    xmlHttp.setRequestHeader("Content-Type", "text/xml");
+    xmlHttp.send( null );
+
+    console.log(xmlHttp.responseText);
+    clickedElement.innerHTML = xmlHttp.responseText;
 });
