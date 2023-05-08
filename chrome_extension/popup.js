9026cc4f5c3dcf6d8f002f84381d0b6bfddd9081
dima <dimafrunza69@gmail.com>
2023-05-08 07:43:11 +0200

    Implemente socket connection

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,10 +1,28 @@
+const socket = new WebSocket("ws://localhost:8000/ws");
+
+socket.onopen = function(event) {
+  console.log("WebSocket connection established.");
+  socket.send("Hello from the extension!");
+};
+
+socket.onmessage = function(event) {
+  const message = event.data;
+  console.log("Message received from the server: ", message);
+};
+
+socket.onclose = function(event) {
+  console.log("WebSocket connection closed.");
+};
+
+socket.onerror = function(error) {
+  console.error("WebSocket error:", error);
+};
+
 document.addEventListener('click', function(e){
     var clickedElement = e.target;
 
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", 'http://127.0.0.1:8000/', false ); 
-    xmlHttp.setRequestHeader("X-PINGOTHER", "pingpong");
-    xmlHttp.setRequestHeader("Content-Type", "text/xml");
     xmlHttp.send( null );
 
     console.log(xmlHttp.responseText);
--- server_side/main.py
+++ server_side/main.py
@@ -1,15 +1,32 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
-
+websocket_connections = set()
 
 @app.get("/")
 def read_root():
     return {"code": "some random code"}
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    websocket_connections.add(websocket)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Process received data from the extension
+    except WebSocketDisconnect:
+        websocket_connections.remove(websocket)
+
+@app.get("/send_message")
+async def send_message(request: Request):
+    message = "Hello from the server!"
+
+    for websocket in websocket_connections:
+        await websocket.send_text(message)
+
+    return {"message": "Message sent to all connected extensions."}
+
