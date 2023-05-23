335e943ee0360cd0015a65fc6a77fc1e94a9387e
dima <dimafrunza69@gmail.com>
2023-05-23 07:31:56 +0200

    Implemented http get from app to backend

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -29,46 +31,64 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   String cameraScanResult = "default";
+  String tmp = "stuff";
+
   void getNext() async {
-    cameraScanResult = (await scanner.scan()).toString();
+    // tmp = (await scanner.scan()).toString();
+    cameraScanResult = (await createAlbum('stuff')).body;
     notifyListeners();
   }
 
   Future<bool> requestFilePermission() async {
     PermissionStatus result;
     result = await Permission.camera.request();
-    if (result.isGranted){
+    // var result2 = await Permission.internet.request();
+    if (result.isGranted) {
       return true;
     }
     return false;
+  }
+
+  Future<http.Response> createAlbum(String title) {
+    return http.get(
+      Uri.parse('http://localhost:8000'),
+      // Uri.parse('http://192.168.1.254:8000'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      // body: jsonEncode(<String, String>{
+      //   'title': title,
+      // }),
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      var appState = context.watch<MyAppState>();
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
 
-      return Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Text('lol'),
-              Text(appState.cameraScanResult),
-              ElevatedButton(
-                onPressed: () {
-                  appState.requestFilePermission().then((value) {
-                    if(value){
-                      appState.getNext();
-                    }
-                  });
-                },
-child: Text('Next'),
-),
-              ],
-              ),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('lol'),
+            Text(appState.cameraScanResult),
+            ElevatedButton(
+              onPressed: () {
+                appState.createAlbum("My album baby");
+                appState.requestFilePermission().then((value) {
+                  if (value) {
+                    appState.getNext();
+                  }
+                });
+              },
+              child: Text('Next'),
             ),
-          );
-    }
+          ],
+        ),
+      ),
+    );
+  }
 }
