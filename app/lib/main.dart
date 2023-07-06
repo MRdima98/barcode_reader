592637c07b02c78fab37a60157c08dfed9d54ce5
MRdima98 <62344480+MRdima98@users.noreply.github.com>
2023-07-06 08:43:05 +0200

    App supports continuous readings (#4)
    * Simple solution
    
    * Flutter upgrade
    
    * Continues reading

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,52 +1,133 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/rendering.dart';
+import 'package:qr_mobile_vision/qr_camera.dart';
+import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:permission_handler/permission_handler.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
   HttpOverrides.global = MyHttpOverrides();
-  runApp(MyApp());
+  debugPaintSizeEnabled = false;
+  runApp(HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyApp());
+  }
 }
 
 class MyApp extends StatefulWidget {
   @override
-    _MyAppState createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-          home: Scaffold(
-            body: Center(
-              child: FilledButton(
-                onPressed: () {
-                createAlbum();
-                },
-                child: Text('Leggi codice'),
-                ),
-              )
-            )
-          );
-    }
+  String? qr;
+  String? controlBit;
+  bool camState = false;
+  bool dirState = false;
 
-  Future<String> _scan() async {
-    await Permission.camera.request();
-    return (await scanner.scan()).toString();
+  @override
+  initState() {
+    super.initState();
   }
 
-  Future<http.Response> createAlbum() async {
+  Future<http.Response> createAlbum(String? string) async {
+    String stuff = "Default";
+    if (string is String){
+      stuff = string;
+    }
+    print(stuff);
+
     return http.post(
       Uri.https('dev.dumitru.fr:80', 'send_message'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'qrCode': await _scan(),
+        'qrCode': stuff,
       }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Back"),
+                Switch(value: dirState, onChanged: (val) => setState(() => dirState = val)),
+                Text("Front"),
+              ],
+            ),
+            Expanded(
+                child: camState
+                    ? Center(
+                        child: SizedBox(
+                          width: 300.0,
+                          height: 600.0,
+                          child: QrCamera(
+                            onError: (context, error) => Text(
+                              error.toString(),
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            cameraDirection: dirState ? CameraDirection.FRONT : CameraDirection.BACK,
+                            qrCodeCallback: (code) {
+                              setState(() {
+                                qr = code;
+                                if (qr != controlBit){
+                                  controlBit = qr;
+                                  createAlbum(controlBit);
+                                }
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                border: Border.all(
+                                  color: Colors.orange,
+                                  width: 10.0,
+                                  style: BorderStyle.solid,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Center(child: Text("Camera inactive"))),
+            Text("QRCODE: $qr"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Text(
+            "on/off",
+            textAlign: TextAlign.center,
+          ),
+          onPressed: () {
+            setState(() {
+              camState = !camState;
+            });
+          }),
     );
   }
 }
