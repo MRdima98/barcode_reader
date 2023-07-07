c1ed134417ee9492c4de9242046bf07e0185cc0c
dima <dimafrunza69@gmail.com>
2023-07-07 08:52:08 +0200

    Added home page to server side

--- server_side/main.py
+++ server_side/main.py
@@ -1,13 +1,21 @@
+import uvicorn
 from typing import Union
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 websocket_connections = set()
 
 @app.get("/")
-def read_root():
-    return {"homepage": "Hello world"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", { "request":request })
+    #return FileResponse('index.html')
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -30,3 +38,8 @@ async def send_message(request: Request):
 
     return {"message": "Message sent to all connected extensions."}
 
+if __name__ == '__main__':
+    uvicorn.run(
+            'main:app', port=443, host='0.0.0.0',
+            ssl_keyfile='/etc/letsencrypt/live/dev.dumitru.fr/privkey.pem',
+            ssl_certfile='/etc/letsencrypt/live/dev.dumitru.fr/cert.pem')
